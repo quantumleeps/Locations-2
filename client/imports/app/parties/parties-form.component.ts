9f3c0ce15b982fe973815d0de06d13bff31fb5ae
Dan Leeper <dan.leeper@gmail.com>
2016-12-08 11:59:45 -0500

    Up to step 5.9

---
+++ client/imports/app/parties/parties-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+ 
+import template from './parties-form.component.html';
+ 
+@Component({
+  selector: 'parties-form',
+  template
+})
+export class PartiesFormComponent implements OnInit {
+    addForm: FormGroup;
+
+    constructor (
+        private formBuilder: FormBuilder
+    ) {}
+
+    ngOnInit () {
+        this.addForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            description: [],
+            location: ['', Validators.required]
+        });
+    }
+}
