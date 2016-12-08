a424880fde9957c113b24aecc783f38f0498490a
Dan Leeper <dan.leeper@gmail.com>
2016-12-08 12:41:47 -0500

    Step 6 complete

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { Parties } from '../../../both/collections/parties.collection';
-import { Party } from '../../../both/models/party.model';
 
 import template from './app.component.html';
 
@@ -10,15 +6,5 @@ import template from './app.component.html';
     selector: 'app',
     template
 })
-export class AppComponent {
 
-    parties: Observable<Party[]>;
-
-    constructor() {
-        this.parties = Parties.find({}).zone()
-    }
-
-    removeParty(party: Party): void {
-        Parties.remove(party._id);
-    }
-}
+export class AppComponent {}
