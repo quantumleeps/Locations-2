38d39888feb87961b55fbaffbc4d6c0de90ce207
Dan Leeper <dan.leeper@gmail.com>
2016-12-08 11:39:12 -0500

    Done with step 4

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
-import template from './app.component.html'
+import { Parties } from '../../../both/collections/parties.collection';
+import { Party } from '../../../both/models/party.model';
+
+import template from './app.component.html';
 
 @Component({
     selector: 'app',
@@ -8,25 +12,9 @@ import template from './app.component.html'
 })
 export class AppComponent {
 
-    parties: any[];
+    parties: Observable<Party[]>;
 
     constructor() {
-        this.parties = [
-            {
-                'name': 'Dubstep-Free Zone',
-                'description': 'Can we please just for an evening not listen to dubstep.',
-                'location': 'Palo Alto'
-            },
-            {
-                'name': 'All dubstep all the time',
-                'description': 'Get it on!',
-                'location': 'Palo Alto'
-            },
-            {
-                'name': 'Savage lounging',
-                'description': 'Leisure suit required. And only fiercest manners.',
-                'location': 'San Francisco'
-            }
-        ];
+        this.parties = Parties.find({}).zone()
     }
 }
